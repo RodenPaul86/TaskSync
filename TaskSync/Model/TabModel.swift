54e8d01098d56c4fa3711a68d07ac653f966452e
Paul Roden Jr <per312@icloud.com>
2024-11-10 09:25:16 -0600

    Designing custom TabBar.

---
+++ TaskSync/Model/TabModel.swift
@@ -0,0 +1,24 @@
+//
+//  Tab.swift
+//  TaskSync
+//
+//  Created by Paul  on 11/10/24.
+//
+
+import SwiftUI
+
+enum TabModel: String, CaseIterable {
+    case home = "house"
+    case search = "magnifyingglass"
+    case notifications = "bell"
+    case settings = "gearshape"
+    
+    var title: String {
+        switch self {
+        case .home: "Home"
+        case .search: "Search"
+        case .notifications: "Notifications"
+        case .settings: "Settings"
+        }
+    }
+}
