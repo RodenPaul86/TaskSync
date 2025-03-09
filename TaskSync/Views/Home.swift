50056cf7d95c03b48c2aa2a7beb51ed97acec7f1
Paul Roden Jr <per312@icloud.com>
2025-03-08 19:35:27 -0600

    Updating the code to fix an issue with the coredata and fixing the UI

---
+++ TaskSync/Views/Home.swift
@@ -0,0 +1,14 @@
+//
+//  Home.swift
+//  TaskSync
+//
+//  Created by Paul  on 3/8/25.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
