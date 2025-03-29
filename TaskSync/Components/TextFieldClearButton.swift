4f85ca8a33f9018c29e3bcd396ee751cbfdc6dc8
Paul Roden Jr <per312@icloud.com>
2025-03-29 13:17:14 -0500

    Added a all clear button for the text fields

---
+++ TaskSync/Components/TextFieldClearButton.swift
@@ -0,0 +1,36 @@
+//
+//  TextFieldClearButton.swift
+//  TaskSync
+//
+//  Created by Paul  on 3/29/25.
+//
+
+import SwiftUI
+
+struct TextFieldClearButton: ViewModifier {
+    @Binding var text: String
+    
+    func body(content: Content) -> some View {
+        ZStack(alignment: .trailing) {
+            content
+            
+            if !text.isEmpty {
+                Button {
+                    text = ""
+                } label: {
+                    Image(systemName: "multiply.circle.fill")
+                        .foregroundStyle(.gray)
+                }
+                .offset(x: 25)
+            }
+        }
+    }
+}
+
+extension View {
+    func clearButton(text: Binding<String>) -> some View {
+        modifier(TextFieldClearButton(text: text))
+            .padding(.trailing, 35) // Extra space for the button
+            .frame(maxWidth: .infinity) // Ensure it takes the full available width
+    }
+}
