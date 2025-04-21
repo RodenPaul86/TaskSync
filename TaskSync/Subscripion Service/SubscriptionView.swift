58b4c35673243c5f503038c0fec8f4ab5c62c3fc
Paul Roden Jr <per312@icloud.com>
2025-04-21 11:06:29 -0500

    Fixed a bug

--- TaskSync/Subscripion Service/SubscriptionView.swift
+++ TaskSync/Subscripion Service/SubscriptionView.swift
@@ -95,7 +95,6 @@ struct SubscriptionView: View {
                     // Annual & Monthly Buttons
                     HStack(spacing: 15) {
                         SubscriptionButton(plan: .annual, selectedPlan: $selectedPlan, offering: currentOffering)
-                        
                         SubscriptionButton(plan: .weekly, selectedPlan: $selectedPlan, offering: currentOffering)
                     }
                     .frame(height: 100)
