b94f174a8104b4d1974fe1fa2380115882a50184
Paul Roden Jr <per312@icloud.com>
2025-06-04 09:05:32 -0500

    Moved the currency formatter

--- TaskSync/Subscripion Service/CurrencyFormatter.swift
+++ TaskSync/Subscripion Service/CurrencyFormatter.swift
@@ -13,20 +13,32 @@ import Combine
 class CurrencyFormatter: NSObject, ObservableObject, @preconcurrency CLLocationManagerDelegate {
     private let locationManager = CLLocationManager()
     @Published var currencyCode: String = "USD" // default fallback
-
+    @Published var convertedAmount: String = ""
+    
+    // Static exchange rates relative to USD
+    private let exchangeRates: [String: Double] = [
+        "USD": 1.0,
+        "EUR": 0.93,
+        "GBP": 0.79,
+        "JPY": 157.12,
+        "CAD": 1.37,
+        "AUD": 1.52,
+        "INR": 83.26
+    ]
+    
     override init() {
         super.init()
         locationManager.delegate = self
         locationManager.requestWhenInUseAuthorization()
         locationManager.startUpdatingLocation()
     }
-
+    
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         guard let location = locations.last else { return }
         fetchCurrencyCode(from: location)
         locationManager.stopUpdatingLocation()
     }
-
+    
     private func fetchCurrencyCode(from location: CLLocation) {
         CLGeocoder().reverseGeocodeLocation(location) { placemarks, _ in
             if let countryCode = placemarks?.first?.isoCountryCode {
@@ -38,12 +50,15 @@ class CurrencyFormatter: NSObject, ObservableObject, @preconcurrency CLLocationM
             }
         }
     }
-
+    
     func format(_ value: Double) -> String {
-        value.formatted(
+        let rate = exchangeRates[currencyCode] ?? 1.0
+        let convertedValue = value * rate
+        
+        return convertedValue.formatted(
             .currency(code: currencyCode)
-                .notation(.compactName)
-                .precision(.fractionLength(0...2))
+            .notation(.compactName)
+            .precision(.fractionLength(0...2))
         )
     }
 }
